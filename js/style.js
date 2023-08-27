f2e5b5bc62566d11642f157fbdd641051f5271a0
Adnan-Rony <adnanrony19@gmail.com>
2023-08-28 01:12:53 +0600

    card done modal

--- js/style.js
+++ js/style.js
@@ -1,4 +1,4 @@
-    const loadPhone =async (searchText) =>{
+    const loadPhone =async (searchText='apple') =>{
         const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
 
         const data=await res.json();
@@ -19,7 +19,7 @@
         // console.log(phones);
        phones.forEach(phone => {
 
-        console.log(phone);
+        // console.log(phone);
         //2.create a div
         const phoneCard=document.createElement('div');
 
@@ -30,8 +30,8 @@
         <div class="card-body">
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-            <button class="btn btn-accent text-white">Buy Now</button>
+            <div class="card-actions justify-center">
+            <button onclick="handleShowDetails('${phone.slug}');ShowDetailsModal.showModal()" class="btn btn-accent text-white">Show Details</button>
             </div>
         </div>
         </div>
@@ -71,15 +71,52 @@
         }
     }
 
+//show details handle
+    const handleShowDetails = async (id) =>
+    {
+        // console.log('click',id);
+        const res=await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+        const data=await res.json();
+     
+        const phone =data.data;
+        showPhoneDetails(phone)
+    }
+
+//
+const showPhoneDetails =(phone)=>
+{
+    const phoneName=document.getElementById('phone name')
+    // phoneName.innerText=phone.name;
+    console.log(phone);
+
+    const showdetailscontainer=document.getElementById('show-details-container');
+    showdetailscontainer.innerHTML=`
+        <div class="flex justify-center"> 
+        <img class="justify-center" src="${phone.image}" alt ="" />
+        </div>
+        <p class="text-2xl font-bold"> ${phone.name} </p>  
+        <p > <span class=" font-semibold" >Stroage: </span> ${phone?.mainFeatures.storage}</p>  
+        <p> <span class=" font-semibold">Display Size: </span> ${phone.mainFeatures.displaySize }</p>  
+        <p> <span class=" font-semibold">chipSet : </span> ${phone.mainFeatures.chipSet }</p> 
+        <p> <span class=" font-semibold">sensors:</span>${phone.mainFeatures.sensors }  </p>  
+        <p> <span class=" font-semibold">storage: </span> ${phone.mainFeatures.storage }</p>  
+        <p> <span class=" font-semibold">memory: </span>${phone.mainFeatures.memory } </p>  
+        <p> <span class=" font-semibold"> </span>Slug:${phone.slug} </p>  
+        <p> <span class=" font-semibold">releaseDate: </span>${phone.releaseDate} </p>  
+    `
 
 
 
 
 
 
+    ShowDetailsModal.showModal();
+}
 
 
-    // loadPhone()
+
+
+loadPhone()
 
     
 
